5eecb898899a6c1c4d410c0473e4f8c75fc70cdd
AlexayStar <66807328+AlexayStar@users.noreply.github.com>
2022-11-16 19:45:59 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+﻿const popup = document.querySelector('.popup');
+const popupForm = document.querySelector('.popup_form');
+const openPopupButtons = document.querySelector('.header_btn');
+const popupClose = document.querySelector('.popup_close');
+
+openPopupButtons.addEventListener('click', (e) => {
+    e.preventDefault();
+    popup.classList.add('popup_active')
+});
+
+popupClose.addEventListener('click', () => {
+    popup.classList.remove('popup_active');
+});
+
+document.addEventListener('click', (e) => {
+    if (e.target == popup) {
+        popup.classList.remove('popup_active');
+    }
+});
