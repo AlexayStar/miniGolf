d2d9078beb9dd1266d2f436de223b4cbb13d9050
AlexayStar <66807328+AlexayStar@users.noreply.github.com>
2022-11-20 09:10:30 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-﻿const popup = document.querySelector('.popup');
+﻿// Popup форма обратной связи для оставления заявки
+const popup = document.querySelector('.popup');
 const popupForm = document.querySelector('.popup_form');
 const openPopupButtons = document.querySelector('.header_btn');
 const popupClose = document.querySelector('.popup_close');
@@ -18,9 +19,22 @@ document.addEventListener('click', (e) => {
     }
 });
 
+// Вызов меню
 const menubBtn = document.querySelector('.menu_btn');
 const menuList = document.querySelector('.menu_list');
 
 menubBtn.addEventListener('click', () => {
+    menubBtn.classList.toggle('menu_btn--active')
     menuList.classList.toggle('menu_list-active')
 });
+
+// Смена местами gallery_item-1 и gallery_item-2
+const galleryInner = document.querySelector('.gallery_inner');
+const galleryItem2 = document.querySelector('.gallery_item-2');
+const pageWidth = window.innerWidth;
+
+console.log(pageWidth);
+
+if (pageWidth <= 779) {
+    galleryInner.prepend(galleryItem2);
+}
